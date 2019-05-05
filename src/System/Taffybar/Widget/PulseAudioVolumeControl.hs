d2f6e18b8c94f0c8a3f38a3db9c97862161e90fe
Ulf Jasper <ulf.jasper@web.de>
2019-05-05 14:46:01 +0200

    Implement dbus connection to pulseaudio

--- src/System/Taffybar/Widget/PulseAudioVolumeControl.hs
+++ src/System/Taffybar/Widget/PulseAudioVolumeControl.hs
@@ -19,8 +19,15 @@ module System.Taffybar.Widget.PulseAudioVolumeControl (
   pulseAudioVolumeControlNew)
 where
 
-import GI.Gtk
-import Control.Monad.IO.Class
+import           GI.Gtk
+import           Control.Concurrent
+import           Control.Monad
+import           Control.Monad.IO.Class
+import           System.Taffybar.Context
+import Data.Word
+
+import DBus
+import DBus.Client
 
 -- $usage
 --
@@ -30,13 +37,56 @@ import Control.Monad.IO.Class
 -- > main = do
 -- >   let vc = pulseAudioVolumeControlNew
 --
--- Now you can use @menu@ as any other Taffybar widget.
+-- Now you can use @vc@ like any other Taffybar widget.
 
 
 -- | Create a new XDG Menu Widget.
-pulseAudioVolumeControlNew :: MonadIO m =>
-                              m GI.Gtk.Widget
+
+pulseAudioVolumeControlNew :: TaffyIO Widget
 pulseAudioVolumeControlNew = do
-  button <- volumeButtonNew
-  _ <- afterScaleButtonValueChanged button $ \value -> print value
-  toWidget button
+  liftIO $ do
+    button <- volumeButtonNew
+    mClient <- setup button
+    case mClient of
+      Nothing -> return ()
+      Just paClient -> do _ <- afterScaleButtonValueChanged button (setVolume paClient)
+                          return ()
+    toWidget button
+
+setup :: VolumeButton -> IO (Maybe Client)
+setup button = do
+  client <- connectSession
+ 
+  eReply <- getPropertyValue client (methodCall "/org/pulseaudio/server_lookup1" "org.PulseAudio.ServerLookup1" "Address")
+            { methodCallDestination = Just "org.PulseAudio1" }
+  case (eReply :: Either MethodError String) of
+    Left e -> do print e
+                 return Nothing
+    Right addressString -> do let Just ad = parseAddress addressString
+                              paClient <- connect ad
+                              enterLoop paClient button
+                              return $ Just paClient
+
+enterLoop :: Client -> VolumeButton -> IO ()
+enterLoop paClient button = 
+  void $ forkIO $ do doMonitor
+
+      where doMonitor = do
+              eReply <- getPropertyValue paClient (methodCall "/org/pulseaudio/core1/sink0" "org.PulseAudio.Core1.Device" "Volume")
+              case (eReply :: Either MethodError [Word32]) of
+                Left err -> print err
+                Right v -> do let vol = ((fromIntegral (foldl (+) 0 v)) / (fromIntegral ((length v) * 65536)))::Double
+                              scaleButtonSetValue button vol
+              threadDelay (1000 * 100)
+              doMonitor
+              return ()
+  
+
+setVolume :: Client -> Double -> IO ()
+setVolume paClient val = do
+  mError <- setPropertyValue paClient (methodCall "/org/pulseaudio/core1/sink0" "org.PulseAudio.Core1.Device" "Volume") $
+    toVariant [(floor $ 65536 * val)::Word32]
+  case mError of
+    Nothing -> return ()
+    Just err -> print err
+  
